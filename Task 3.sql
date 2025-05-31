695aedd80fa347b1dd04a4e016a7670103af5dc3
Ksenia <113821999+TurinaKsenia@users.noreply.github.com>
2025-05-31 23:58:43 +0300

    Update Task 3.sql

--- Task 3.sql
+++ Task 3.sql
@@ -1,3 +1,6 @@
+--Определить книгу, которая была наиболее популярной весной 2000 года
+-----------------------------------------------------------------------
+--Выбираем все книги, которые брали весной 2000 года и для каждой группы считаем количсетво взятых раз
 with popular AS
 (
 SELECT group_id, count(group_id) as kol
@@ -7,7 +10,7 @@ ON bd.book_id=b.book_id
 WHERE distribution_date BETWEEN '2000-03-01' AND '2000-05-31'
 GROUP BY group_id
 )
-
+--Находим те строки, в которых количество находится в списке максимальных
 SELECT bg.author, bg.name_book, p.kol 
 FROM book_groups bg
 INNER JOIN popular p
