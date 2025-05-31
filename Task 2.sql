a1ad4bf734373cdf4f808b388955616a39a0bfda
Ksenia <113821999+TurinaKsenia@users.noreply.github.com>
2025-05-31 23:56:57 +0300

    Update Task 2.sql

--- Task 2.sql
+++ Task 2.sql
@@ -1,3 +1,6 @@
+--Выбрать читателей, которые имеют задолженность более 4-х месяцев
+--Рассматриваем 2 вида: те, у которых когда-то была задолженность и те, у которых сейчас есть долг
+-- INTERVAL - тип данных, который позволяет точно сравнивать даты в зависимости от года, месяца
 SELECT fio, CURRENT_DATE::timestamp-distribution_date::timestamp as real_arrears, null as hystory_arrears FROM readers r
 INNER JOIN (
 SELECT reader_id, distribution_date, refund_date
@@ -11,4 +14,3 @@ SELECT reader_id, distribution_date, refund_date
 FROM book_distribution bd
 WHERE (refund_date IS NOT NULL and refund_date::timestamp-distribution_date::timestamp > INTERVAL'4 months')) as ququ
 ON r.reader_id=ququ.reader_id
-
